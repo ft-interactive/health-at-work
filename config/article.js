1643048b223e1ae82d9faf70865687acd849da6d
David Blood <davidbjourno@users.noreply.github.com>
2017-09-12 15:35:31 +0100

    URL

--- config/article.js
+++ config/article.js
@@ -5,7 +5,7 @@ export default () => ({ // eslint-disable-line
 
   // canonical URL of the published page
   //  get filled in by the ./configure script
-  url: 'https://ig.ft.com/health-at-work/2017',
+  url: 'https://ig.ft.com/special-reports/health-work/',
 
   // To set an exact publish date do this:
   //       new Date('2016-05-17T17:11:22Z')
