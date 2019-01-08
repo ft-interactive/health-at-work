67e5e4a40871d56c5c14106a614567ff5b58b66e
David Blood <9371095+davidbjourno@users.noreply.github.com>
2019-01-08 11:37:44 +0000

    Tweak gutters

--- client/components/app/chart/small-multiple-line/index.js
+++ client/components/app/chart/small-multiple-line/index.js
@@ -64,7 +64,7 @@ class SmallMultipleLine extends PureComponent {
         case 'S':
         case 'default':
         default:
-          return 20;
+          return 18;
       }
     })(layout);
 
